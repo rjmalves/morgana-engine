77eb188b7429f4ec7cedb8e90e0d547ba1dc06c3
Rogerio Alves <rogerioalves.ee@gmail.com>
2024-03-04 10:19:24 -0300

    update version

--- morgana_engine/__init__.py
+++ morgana_engine/__init__.py
@@ -6,6 +6,6 @@ morgana_engine is a Python module for handling partitioned data
 using SQL queries.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .engine import select_lambda_endpoint  # noqa
